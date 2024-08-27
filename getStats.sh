74ef023b68783cc715ca9bfb7499e86c8b22d03a
Maurice Lim <mau.mauricelim@gmail.com>
2024-08-27 09:39:24 +0000

    Test remove multiline

--- getStats.sh
+++ getStats.sh
@@ -3,9 +3,7 @@ stats=$(curl -sL https://www.vimgolf.com/45533/mau-mauricelim)
 CHALLENGE=$(git rev-parse --show-toplevel)/Played_Challenges
 
 echo "$stats" > "$CHALLENGE/inp"
-out=$(echo "$stats" |\
-    sed '/^<!/,/^<h5/d ; /grid_5/,/html/d ; /^<\/h/,/^<ul/d ; /^<\/u/,/^<h/d ; s/"/"https:\/\/www.vimgolf.com/ ; s/^<a.*"\(.*\)">\(.*\)<.* \(\d\+\).*/[\2](\1)|\3/ ; s/<a.*"\(.*\)">\(\d\+\).*a>/[\2](\1)/ ; s/^<.*<b>\(.*\)<\/b.*/\1/' |\
-    paste -d"|" - - - - -)
+out=$(echo "$stats" | sed '/^<!/,/^<h5/d ; /grid_5/,/html/d ; /^<\/h/,/^<ul/d ; /^<\/u/,/^<h/d ; s/"/"https:\/\/www.vimgolf.com/ ; s/^<a.*"\(.*\)">\(.*\)<.* \(\d\+\).*/[\2](\1)|\3/ ; s/<a.*"\(.*\)">\(\d\+\).*a>/[\2](\1)/ ; s/^<.*<b>\(.*\)<\/b.*/\1/' | paste -d"|" - - - - -)
 
 { echo -e "Played Challenges|Entries|Best score|Best player score|Position|Number of attempts\n-|-|-|-|-|-"; echo "$out"; } > "$CHALLENGE/out"
 sed -i 's/\(.*\)/|\1|/' "$CHALLENGE/out"
