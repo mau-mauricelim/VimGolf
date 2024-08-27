b0b7f328f6d2688834a88cfa42fecb5271541d09
Maurice Lim <mau.mauricelim@gmail.com>
2024-08-27 10:03:38 +0000

    Test split workflow

--- getStats.sh
+++ getStats.sh
@@ -4,14 +4,14 @@ CHALLENGE=$(git rev-parse --show-toplevel)/Played_Challenges
 
 echo "$stats" > "$CHALLENGE/inp"
 out=$(echo "$stats" |\
-    sed '/^<!/,/^<h5/d' |\
-    sed '/grid_5/,/html/d' |\
-    sed '/^<\/h/,/^<ul/d' |\
-    sed '/^<\/u/,/^<h/d' |\
-    sed 's/"/"https:\/\/www.vimgolf.com/' |\
-    sed 's/^<a.*"\(.*\)">\(.*\)<.* \(\d\+\).*/[\2](\1)|\3/' |\
-    sed 's/<a.*"\(.*\)">\(\d\+\).*a>/[\2](\1)/' |\
-    sed 's/^<.*<b>\(.*\)<\/b.*/\1/' |\
+    sed '/^<!/,/^<h5/d ;\
+        /grid_5/,/html/d ;\
+        /^<\/h/,/^<ul/d ;\
+        /^<\/u/,/^<h/d ;\
+        s/"/"https:\/\/www.vimgolf.com/ ;\
+        s/^<a.*"\(.*\)">\(.*\)<.* \(\d\+\).*/[\2](\1)|\3/ ;\
+        s/<a.*"\(.*\)">\(\d\+\).*a>/[\2](\1)/ ;\
+        s/^<.*<b>\(.*\)<\/b.*/\1/' |\
     paste -d"|" - - - - -)
 
 { echo -e "Played Challenges|Entries|Best score|Best player score|Position|Number of attempts\n-|-|-|-|-|-"; echo "$out"; } > "$CHALLENGE/out"
